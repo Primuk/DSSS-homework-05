20b5606473b752e6e84dcab54d2f68ea1b4b942a
Primula Mukherjee <34391753+Primuk@users.noreply.github.com>
2022-12-02 23:33:14 +0100

    add license

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     version = "0.1.1",
     author = "Primula Mukherjee",
     author_email = "primula.mukherjee@fau.de",
+    license='Apache-2.0 license',
     packages = find_packages(),
     install_requires = ["numpy", "turtles"],
  )
